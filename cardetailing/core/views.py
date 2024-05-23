78790ced6598b0199bd5666a17287caf3a5ba38f
Sebastian Żądło <seba0071@interia.pl>
2024-05-23 16:58:50 +0200

    Initial commit

---
+++ cardetailing/core/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserCreateSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serialized = UserCreateSerializer(data=request.data)
+        if serialized.is_valid():
+            user = User.objects.filter(username=serialized.initial_data['username']).first()
+            if user:
+                return Response({"message": "User already exists!"}, status=status.HTTP_400_BAD_REQUEST)
+
+            User.objects.create_user(
+                username=serialized.initial_data['username'],
+                email=serialized.initial_data['email'],
+                password=serialized.initial_data['password']
+            )
+            return Response({"message": "created"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
